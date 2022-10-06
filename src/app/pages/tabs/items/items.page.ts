c55a45c2a71db006adb53d9e98268136ca4c9145
Alberto Sánchez Periñán <asanchez@depid.local>
2022-10-06 13:23:04 +0200

    cartData

--- src/app/pages/tabs/items/items.page.ts
+++ src/app/pages/tabs/items/items.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-items',
@@ -13,6 +14,8 @@ export class ItemsPage implements OnInit {
   data: any = {};
   items: any[] = [];
   veg: boolean = false;
+  cartData: any = {};
+  storeData: any = {}
 
   restaurants = [
     {
@@ -119,7 +122,8 @@ export class ItemsPage implements OnInit {
 
   constructor(
     private navCtrl: NavController,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -135,12 +139,35 @@ export class ItemsPage implements OnInit {
     });
   }
 
-  getItems() {
+  getCart() {
+    return Preferences.get({key: 'cart'});
+  }
+
+  async getItems() {
     this.data = {};
+    this.cartData = {};
+    this.storeData = {};
     let data: any = this.restaurants.filter(x => x.uid === this.id);
     this.data = data[0];
-    this.items = this.allItems;
+    //this.categories = this.categories.filter(x => x.uid === this.id);
+    this.items = this.allItems;//.filter(x => x.uid === this.id);
     console.log('restaurant: ', this.data);
+    let cart: any = await this.getCart();
+    console.log(cart);
+    if(cart?.value) {
+      this.storeData = JSON.parse(cart.value);
+      console.log(this.storeData);
+      if(this.id == this.storeData.restaurant.uid && this.allItems.length > 0) {
+        this.allItems.forEach((element:any) => {
+          this.storeData.item.forEach(ele => {
+            if(element.id != ele.id) return;
+            element.quantity = ele.quantity;
+          });
+        });
+      }
+      this.cartData.totalItem = this.storeData.totalItem;
+      this.cartData.totalPrice = this.storeData.totalPrice;
+    }
   }
 
   getCuisine(cuisine) {
@@ -149,8 +176,74 @@ export class ItemsPage implements OnInit {
 
   vegOnly(event) {
     console.log(event.detail.checked);
+    this.items = [];
+    if(event.detail.checked == true) this.items = this.allItems.filter(x => x.veg === true );
+    else this.items = this.allItems;
+    console.log('items: ', this.items);
+  }
+
+  quantityPlus(item, index) {
+    try {
+      console.log(this.items[index]);
+      if(!this.items[index].quantity || this.items[index].quantity == 0) {
+        this.items[index].quantity = 1;
+        this.calculate();
+      } else {
+        this.items[index].quantity += 1; // this.items[index].quantity = this.items[index].quantity + 1
+        this.calculate();
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  quantityMinus(item, index) {
+    if(this.items[index].quantity !== 0) {
+      this.items[index].quantity -= 1; // this.items[index].quantity = this.items[index].quantity - 1
+    } else {
+      this.items[index].quantity = 0;
+    }
+    this.calculate();
+  }
+
+  calculate() {
+    console.log(this.items);
+    this.cartData.items = [];
+    let item = this.items.filter(x => x.quantity > 0);
+    console.log('added items: ', item);
+    this.cartData.items = item;
+    this.cartData.totalPrice = 0;
+    this.cartData.totalItem = 0;
+    item.forEach(element => {
+      this.cartData.totalItem += element.quantity;
+      this.cartData.totalPrice += (parseFloat(element.price) * parseFloat(element.quantity));
+    });
+    this.cartData.totalPrice = parseFloat(this.cartData.totalPrice).toFixed(2);
+    if(this.cartData.totalItem == 0) {
+      this.cartData.totalItem = 0;
+      this.cartData.totalPrice = 0;
+    }
+    console.log('cart: ', this.cartData);
+  }
+
+  async saveToCart() {
+    try {
+      this.cartData.restaurant = {};
+      this.cartData.restaurant = this.data;
+      console.log('cartData', this.cartData);
+      await Preferences.set({
+        key:'cart',
+        value: JSON.stringify(this.cartData)
+      })
+    } catch (e) {
+      console.log(e)
+    }
 
   }
 
+  viewCart() {
+    if(this.cartData.items && this.cartData.items.length > 0) this.saveToCart();
+    //this.router.navigate([this.router.url + '/cart'])
+  }
 
 }
